b10e7db2414b93b064bc01ae85641ec48e585a9d
Carol Yao <c.y-@hotmail.com>
2016-06-07 17:42:32 -0400

    Added repeat string method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -5,3 +5,7 @@ end
 def shout(input)
   input.upcase
 end
+
+def repeat(input, times = 2) # note that times = 2 default if user doesn't input the amount of times to repeat
+  [input] * times * " " # note that something * times produces an array, but then * " " converts it into a string
+end
