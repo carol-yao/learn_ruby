ba02045f45274576eb14def3d24fd8c726317530
Carol Yao <c.y-@hotmail.com>
2016-06-07 17:45:59 -0400

    Added start_of_word method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -9,3 +9,7 @@ end
 def repeat(input, times = 2) # note that times = 2 default if user doesn't input the amount of times to repeat
   [input] * times * " " # note that something * times produces an array, but then * " " converts it into a string
 end
+
+def start_of_word(input, letters)
+  input[0...letters] # note that if string = "hello", then string[0..1] will give str (so exclude last letter by 0...1)
+end
