8ef2bd2b03b14f8c46f8b654c43d59b32cb315af
Carol Yao <c.y-@hotmail.com>
2016-06-08 16:49:58 -0400

    Created class Friend and added greet method

--- 05_hello_friend/test_hello_friend.rb
+++ 05_hello_friend/test_hello_friend.rb
@@ -3,7 +3,6 @@ require 'friend'
 
 class TestFriend < MiniTest::Test
   # 'setup' gets run before each individual test
-  # We can set up variables that we reuse here
   def setup
     @friend = Friend.new
   end
@@ -16,12 +15,5 @@ class TestFriend < MiniTest::Test
   def test_greets_no_one
     assert_equal "Hello !", @friend.greet
 
-    # Hint: remember default values?
-    #
-    # def greet(name = ?)
-    #   # your code here
-    # end
-    #
-    # What should ? be equal to?
   end
 end
