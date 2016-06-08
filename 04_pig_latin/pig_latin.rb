96415ed82039ddf26b86bb245925ebdde8158c98
Carol Yao <c.y-@hotmail.com>
2016-06-08 16:40:44 -0400

    Revised method to remove qu to the end as well

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,13 +1,23 @@
-def translate(word)
+def translate(string)
   alphabet = ("a".."z").to_a
   vowels = ['a','i','e','o','u']
   consonants = (alphabet - vowels) << "qu"
-  ending = []
-  beginning = word
+  words = string.split(" ")
+  sentence = []
 
-  while word.start_with? *consonants
-    ending << beginning.split("").shift
-    beginning.slice!(0)
+  words.each do |word|
+    beginning = word
+    ending = []
+    while word.start_with? *consonants
+      if word.start_with?('qu')
+      ending << "qu"
+      beginning.slice!(0, 2)
+      break
+    end
+      ending << beginning.split("").shift
+      beginning.slice!(0)
+    end
+    sentence << beginning + ending.join + "ay"
   end
-   beginning + ending.join + "ay"
+  sentence.join(" ")
 end
